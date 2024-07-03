264fe36d566ed6b878cdc277c30e368622676776
yanglinfang <yanglinfang@users.noreply.github.com>
2024-07-02 22:22:39 -0700

    add docker

--- npotechnologies_website/webpage.py
+++ npotechnologies_website/webpage.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
@@ -8,6 +9,14 @@ def get_data():
         'message': 'Hello from Flask!'
     }
     return jsonify(data)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve_react_app(path):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
