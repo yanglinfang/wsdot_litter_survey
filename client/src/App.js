00600d84e198f317d9c7ea0d074d77a5f7a7b104
yanglinfang <yanglinfang@users.noreply.github.com>
2024-07-02 22:18:57 -0700

    Set up React frontend

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios.get('/api/data')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the data!', error);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>{data ? data.message : 'Loading test...'}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
