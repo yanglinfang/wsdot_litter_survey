cf8cd519987739348e478d0951d9643f11fee4f8
yanglinfang <yanglinfang@users.noreply.github.com>
2024-07-02 22:07:19 -0700

    Add flask first webapp page

--- npotechnologies_website/webpage.py
+++ npotechnologies_website/webpage.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, World!"
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = {
+        'message': 'Hello from Flask!'
+    }
+    return jsonify(data)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
